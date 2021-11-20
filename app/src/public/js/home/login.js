61716182c204989301389bcd14f9a10f539cd6ba
정해빈 <sunlight@jeonghaebin-ui-MacBook-Pro.local>
2021-11-20 21:21:58 +0900

    DOM을 이용한 객체저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,14 @@
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+pwd = document.querySelector("#pwd"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+    console.log(req);
+};
+
